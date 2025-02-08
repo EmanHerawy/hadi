9de2fc3d20eb1b6dd88b8206fd737bcc33a99d1b
emanherawy <eman.herawy@gmaail.com>
2025-02-08 21:04:17 +0100

    add basic erc20 token

--- halalScannerToken/script/HalaScannerToken.sol
+++ halalScannerToken/script/HalaScannerToken.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {HalaScannerToken} from "../src/HalaScannerToken.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract HalaScannerTokenScript is Script {
+    HalaScannerToken public halaScannerToken;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        halaScannerToken = new HalaScannerToken();
 
         vm.stopBroadcast();
     }
